c7d47f607689878627ceb7d310bb5fda36f7bbd8
Daniel (Laptop Win10) <perrout@gmail.com>
2020-01-13 11:56:32 -0300

    V160

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f587e8497d5179ee445251896dd3555.js"
+  "/precache-manifest.e09cb49b54b2738e82358bade7c6071e.js"
 );
 
 workbox.clientsClaim();
--- client/src/app/homeTemplate.jsx
+++ client/src/app/homeTemplate.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React from "react";
 
-const HomeTemplate = (props) => {
-  const { user } = props
+const HomeTemplate = props => {
+  const { user } = props;
   return (
     <div>
-      <div className="card-panel" style={{ backgroundImage: `url(${"/images/home_bg.jpg"})` }}>
-        <div className="container" style={{ paddingBottom: '0.5%' }}>
-          <h1 className="display-3"> <strong>
-            <span style={{ color: 'white' }} >Anuência Digital</span> </strong> </h1>
-          <h6 style={{ color: 'white', fontWeight: '500' }}>
-            Versão 1.5.7 - 19/07/19
+      <div
+        className="card-panel"
+        style={{ backgroundImage: `url(${"/images/home_bg.jpg"})` }}
+      >
+        <div className="container" style={{ paddingBottom: "0.5%" }}>
+          <h1 className="display-3">
+            {" "}
+            <strong>
+              <span style={{ color: "white" }}>Anuência Digital</span>{" "}
+            </strong>{" "}
+          </h1>
+          <h6 style={{ color: "white", fontWeight: "500" }}>
+            Versão 1.6.0 - 13/01/2020
           </h6>
           <br />
-          <h4 style={{ color: 'white', fontWeight: '500' }}>
+          <h4 style={{ color: "white", fontWeight: "500" }}>
             Olá, {user.name} {user.surName}!
           </h4>
         </div>
       </div>
       <div className="container">
-        <div className="row">
-          {props.children}
-        </div>
+        <div className="row">{props.children}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomeTemplate;
